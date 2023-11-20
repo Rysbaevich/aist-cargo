8ad42584407485e477a978880eeb9d0d6c0de91c
Нурдөөлөт <r.nurdoolot@gmail.com>
2023-11-20 15:04:00 +0600

    First commit

---
+++ src/main/java/kg/rysbaevich/aistcargo/AistCargoApplication.java
@@ -0,0 +1,13 @@
+package kg.rysbaevich.aistcargo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AistCargoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AistCargoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/rysbaevich/aistcargo/AistCargoApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.rysbaevich.aistcargo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AistCargoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
